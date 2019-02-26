9a7b290c6966b5bbd87f47b76f1992a02e5b95f1
xie-james72 <xie.james@queensu.ca>
2019-02-26 17:09:16 -0500

    updated preprocessing to fix bugs

--- Preprocessing.py
+++ Preprocessing.py
@@ -20,10 +20,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import ndimage
 
-ball = cv2.imread('Test.jpg',0)
+ball = cv2.imread('Test.jpg',1)
+bg = cv2.imread('Test.jpg',1)
 
 def circleFind(img):
-    #if colour, then grayscale the image
+    #if colour, then grayscale the image   
     if len(img.shape) == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
@@ -62,7 +63,7 @@ def ballCrop(img, cir):
     return img
     
 def bgCrop(img):
-    s = min(img.shape)
+    s = min(img.shape[0:1])
     img = img[0:s,0:s]
     return img
     
@@ -90,37 +91,51 @@ def adjust_brightness(ball_img, bg_img):
 def ball_embed(ball_img, bg_img):
     #Accepts a colour image containing a tennis ball with a white background and embeds the tennis ball into a different background
     #Outputs both the new background image with the tennis ball and a mask
-    
     #Convert background white pixels to black, then erode to remove white edge
     kernel = np.ones((5,5),np.uint8)
     gray = cv2.cvtColor(ball_img, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray,254,255,cv2.THRESH_BINARY)
     ball_img[thresh == 255] = 0
-    ball_img = cv2.erode(mask,kernel,iterations = 1)
+    ball_img = cv2.erode(ball_img,kernel,iterations = 1)
     
     #Scale and rotate the ball by random amounts
-    rescale = np.random.random
-    rotate = 360*np.random.random
-    ball_img = cv2.resize(ball_img, fx=rescale, fy=rescale, interpolation = cv2.INTER_CUBIC)
+    rescale = np.random.random()
+    rotate = 360.0*np.random.random()
+    ball_img = cv2.resize(ball_img, (0,0), fx=rescale, fy=rescale, interpolation = cv2.INTER_CUBIC)
+    rows,cols,_ = ball_img.shape
     M = cv2.getRotationMatrix2D((cols/2,rows/2),rotate,1)
-    ball_img = cv2.warpAffine(ball_img,M,(128,128))
+    ball_img = cv2.warpAffine(ball_img,M,(cols,rows))
     
     #Embed ball randomly into background
-    w,h,_ = bg_img.shape
-    offsetY = np.random.random_integers(0, h)
-    offsetX = np.random.random_integers(0, w)
-    bg_img[offsetY:offsetY+h,offsetX:offsetX+w] = ball_img
+    h,w,_ = bg_img.shape
+    rows,cols,_ = ball_img.shape
+    offsetY = np.random.randint(0, h)
+    offsetX = np.random.randint(0, w)
+    bg_img[offsetY:offsetY+rows,offsetX:offsetX+cols,:] = ball_img
     
-    mask = mask_Maker(bg_img, offsetY, offsetX)
-    
+    mask = mask_Maker(bg_img, ball_img, offsetY, offsetX)
     bg_img = cv2.resize(bg_img, (128, 128), interpolation = cv2.INTER_CUBIC)
     
     return bg_img, mask
 
-def mask_Maker(bg_img, offsetY, offsetX):
-    blank = np.zeros(bg_img.shape, dtype=int)
-    blank[offsetY:offsetY+h,offsetX:offsetX+w] = ball_img
-    ball_mask = cv2.threshold(blank,1,255,cv2.THRESH_BINARY)
+def mask_Maker(bg_img, ball_img, offsetY, offsetX):
+    blank = np.zeros(bg_img.shape, dtype=np.uint8)
+    
+    h,w,_ = ball_img.shape
+    blank[offsetY:offsetY+h,offsetX:offsetX+w,:] = ball_img
+    blank = cv2.cvtColor(blank, cv2.COLOR_BGR2GRAY)
+    _, ball_mask = cv2.threshold(blank,1,255,cv2.THRESH_BINARY)
     ball_mask = cv2.resize(ball_mask, (128, 128), interpolation = cv2.INTER_CUBIC)
     
     return ball_mask
+
+cir = circleFind(ball)
+ball = ballCrop(ball,cir)
+bg = bgCrop(bg)
+ball = adjust_brightness(ball, bg)
+bg, mask = ball_embed(ball, bg)
+
+cv2.imshow('0',bg)
+cv2.waitKey(0)
+cv2.imshow('0',mask)
+cv2.waitKey(0)
